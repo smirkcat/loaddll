e62932f80d1f0f7bc8660428e493fd63995e0173
smirklijie <smirkliie@qq.com>
2016-05-27 14:58:25 +0800

    完善解压动态库和加载动态的scala代码

--- src/main/java/com/inspur/loaddll/JarDllScala.scala
+++ src/main/java/com/inspur/loaddll/JarDllScala.scala
@@ -2,9 +2,12 @@ package com.inspur.loaddll
 
 import java.io._
 import scala.util.control._
+import scala.collection.mutable.ListBuffer
+
 
 //如果没有scala环境编译，请注释本文件
 object JarDllScala {
+  //注java代码是参考了javacpp里面源码获取tempDir获取的方法，scala采用static来初始化一个不能改变的变量
   val tempDir: File = {
     val tmpdir = new File(System.getProperty("java.io.tmpdir"))
     val loop = new Breaks
@@ -24,7 +27,7 @@ object JarDllScala {
   }
   val systemType: String = System.getProperty("os.name")
   var libExtension: String = if (systemType.toLowerCase().indexOf("win") != -1) { ".dll" } else ".so"
-  def rootPath(cls:Class[_]): String = {
+  def rootPath(cls: Class[_]): String = {
     var rootPath = cls.getResource("/").getFile()
     // 特别注意rootPath返回有斜杠。linux下不需要去掉，windows需要去掉
     if ((systemType.toLowerCase().indexOf("win") != -1)) {
@@ -33,8 +36,35 @@ object JarDllScala {
     }
     rootPath
   }
-  def loadlib(libName:String, dllpath:String, cls:Class[_]){
-    
+  def loadlib(libName: String, dllpath: String, cls: Class[_]) {
+    val libFullName = dllpath + libName + libExtension
+    //java代码有一个获取tempDir的过程
+    val filepath = tempDir.getAbsolutePath() + "/" + libName + libExtension
+    var extractedLibFile = new File(filepath)
+    if (!extractedLibFile.exists()) {
+      try {
+        val in = cls.getResourceAsStream(libFullName)
+        val reader = new BufferedInputStream(in)
+        val writer = new FileOutputStream(extractedLibFile)
+        //参考下面网址
+        //http://wtqq520.iteye.com/blog/1540433
+        //http://blog.csdn.net/lyrebing/article/details/20362227
+        var buf = ListBuffer[Byte]()
+        var b = reader.read()
+        while (b != -1) {
+          buf.append(b.byteValue)
+          b = reader.read()
+        }
+      } catch {
+        case e: IOException => {
+          if (!extractedLibFile.exists()) {
+            extractedLibFile.delete();
+          }
+          throw e
+        }
+      }
+    }
+    System.load(extractedLibFile.toString());
   }
 }
 
